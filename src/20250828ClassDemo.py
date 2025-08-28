44de086fdd4d913c630dfa13b2cf2cd50104cc28
Seth McNeill <mcneills@morningside.edu>
2025-08-28 12:34:28 -0500

    Includes examples from class

--- src/20250828ClassDemo.py
+++ src/20250828ClassDemo.py
@@ -7,6 +7,22 @@ This demonstrates built in data structures and control commands in Python
 | Date: 2025 August 28
 """
 
+def myUpperCase(myString):
+    """
+    This converts a string to all uppercase
+
+    Parameters
+    --------
+    myString : str
+      A string to be made all uppercase
+    
+    Returns
+    --------
+    str
+      An uppercase version of myString
+    """
+    return myString.upper()
+
 
 if __name__ == "__main__":
     """
@@ -14,3 +30,36 @@ if __name__ == "__main__":
     not if it is imported
     """
     print("hello world")
+
+    mStr = "This is 3 my test 5 String."
+    print(mStr)
+    uStr = myUpperCase(mStr)
+    print(uStr)
+
+    mySet = set(mStr)
+    myList = list(mySet)
+    type(mySet)
+    type(myList)
+
+    for n in range(len(mStr)):
+      if n%2:  # odd numbered indexes
+        print(mStr[n].lower(), end='')
+      else:
+        print(mStr[n].upper(), end='')
+    print()
+
+    for letter in mStr:
+      if letter in 'aeiou':
+        print(letter.upper(), end='')
+      else:
+        print(letter.lower(), end='')
+    print()
+
+    for letter in mStr:
+      if letter in 'aeiou':
+        print(letter.upper(), end='')
+      elif letter in '1234567890':
+        print('#', end='')
+      else:
+        print(letter.lower(), end='')
+    print()
