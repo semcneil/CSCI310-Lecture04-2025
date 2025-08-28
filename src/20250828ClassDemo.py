58e59beaff2a41929f478f394d12bfab48291c0c
Seth McNeill <mcneills@morningside.edu>
2025-08-28 12:05:16 -0500

    Initial Python file that runs

---
+++ src/20250828ClassDemo.py
@@ -0,0 +1,16 @@
+"""
+20250828ClassDemo.py
+====================================
+This demonstrates built in data structures and control commands in Python
+
+| Author: Seth McNeill
+| Date: 2025 August 28
+"""
+
+
+if __name__ == "__main__":
+    """
+    Code inside this if statement will only run if called as a script
+    not if it is imported
+    """
+    print("hello world")
